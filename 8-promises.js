d5749a03534dd36e6c0a4ff84e15925ff6d71cd0
Dejalma Arantes <dejalma.arantes>
2020-03-04 13:18:56 +0000

    promise chaining

--- 8-promises.js
+++ 8-promises.js
@@ -3,8 +3,8 @@ const chalk = require('chalk')
 const doWorkPrimise = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve([7, 4, 1])
-        reject('Things went wrong!')
-    }, 3000)
+        //reject('Things went wrong!')
+    }, 1000)
 })
 
 doWorkPrimise.then((result) => {
@@ -14,3 +14,35 @@ doWorkPrimise.then((result) => {
 })
 
 console.log(chalk.yellow.bold.inverse('Bernando Arantes'))
+
+// promises chaining
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+add(1, 2).then((sum) => {
+    console.log(chalk.green(sum))
+
+    add(sum, 3).then((sum) => {
+        console.log(chalk.green(sum))
+
+    }).catch((error) => {
+        console.log(chalk.red(error))
+    })
+}).catch((error) => {
+    console.log(chalk.red(error))
+})
+
+add(4, 5).then((sum) => {
+    console.log(chalk.green(sum))
+    return add(sum, 6)
+}).then((sum) => {
+    console.log(chalk.green(sum))
+}).catch((error) => {
+    console.log(chalk.red(error))
+})
